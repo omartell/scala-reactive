927f548f2fd66fcdb05c43d124b331d53daac08b
Oliver Martell <oliver.martell@gmail.com>
2014-03-24 23:00:49 +0000

    Playing with futures by implementing a mini node server

---
+++ nodescala/src/test/scala/nodescala/tests.scala
@@ -0,0 +1,239 @@
+package nodescala
+
+
+
+import scala.language.postfixOps
+import scala.util.{Try, Success, Failure}
+import scala.collection._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.async.Async.{async, await}
+import org.scalatest._
+import NodeScala._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class NodeScalaSuite extends FunSuite {
+
+  test("A Future should always be created") {
+    val always = Future.always(517)
+
+    assert(Await.result(always, 0 nanos) == 517)
+  }
+
+  test("A Future should never be created") {
+    val never = Future.never[Int]
+
+    try {
+      Await.result(never, 1 second)
+      assert(false)
+    } catch {
+      case t: TimeoutException => // ok!
+    }
+  }
+
+  test("A list of futures combined into a future list"){
+    val fa = future{ 1 }
+    val fb = future{ 2 }
+
+    val all = Future.all(List(fa,fb))
+
+
+    val list = Await.result(all, 1 second)
+    assert(list == List(1,2))
+  }
+
+  test("When any of the futures is completed the combined future is completed"){
+    val fa = future{ blocking{ Thread.sleep(1000) }; 1 }
+    val fb = future{ 2 }
+
+    val any = Future.any(List(fa,fb))
+
+    val result = Await.result(any, 1 second)
+    assert(result == 2)
+  }
+
+  test("Delay"){
+    val df = Future.delay(Duration("3 seconds"))
+
+    df onComplete { case _ => println("Future Done")}
+
+    Thread.sleep(Duration("4 seconds").toSeconds)
+
+    assert(df.isCompleted == true)
+  }
+
+  test("If the first futures fails, then the combined future fails"){
+    val fb = future{ throw new NoSuchElementException }
+
+    val any = Future.any(List(fb))
+
+    try {
+      val result = Await.result(any, 1 second)
+      assert(false)
+    }
+    catch{
+      case t: NoSuchElementException => //ok
+    }
+  }
+
+  test("Now"){
+    val fa = future{ 1 }
+    assert(fa.now == 1 )
+
+    val fb = future{ blocking{ Thread.sleep(2000)} }
+
+    try{
+      fb.now
+      assert(false)
+    }
+    catch{
+      case t: NoSuchElementException => //ok!
+    }
+  }
+
+  test("ContinueWith"){
+    val fa = future{ 1000 } continueWith ( f => 2 )
+    assert(Await.result(fa, 1 second) == 2 )
+  }
+
+  test("CancellationTokenSource should allow stopping the computation") {
+    val cts = CancellationTokenSource()
+    val ct = cts.cancellationToken
+    val p = Promise[String]()
+
+    async {
+      while (ct.nonCancelled) {
+        // do work
+      }
+
+      p.success("done")
+    }
+
+    cts.unsubscribe()
+    assert(Await.result(p.future, 1 second) == "done")
+  }
+
+  test("Unsubscribe"){
+    pending
+    val working = Future.run() { ct =>
+      Future {
+        while (ct.nonCancelled) {
+          println("working")
+        }
+        println("done")
+      }
+    }
+
+    Future.delay(1 second) onSuccess {
+      case _ => working.unsubscribe()
+    }
+  }
+
+  class DummyExchange(val request: Request) extends Exchange {
+    @volatile var response = ""
+    val loaded = Promise[String]()
+    def write(s: String) {
+      response += s
+    }
+    def close() {
+      loaded.success(response)
+    }
+  }
+
+  class DummyListener(val port: Int, val relativePath: String) extends NodeScala.Listener {
+    self =>
+
+    @volatile private var started = false
+    var handler: Exchange => Unit = null
+
+    def createContext(h: Exchange => Unit) = this.synchronized {
+      assert(started, "is server started?")
+      handler = h
+    }
+
+    def removeContext() = this.synchronized {
+      assert(started, "is server started?")
+      handler = null
+    }
+
+    def start() = self.synchronized {
+      started = true
+      new Subscription {
+        def unsubscribe() = self.synchronized {
+          started = false
+        }
+      }
+    }
+
+    def emit(req: Request) = {
+      val exchange = new DummyExchange(req)
+      if (handler != null) handler(exchange)
+      exchange
+    }
+  }
+
+  class DummyServer(val port: Int) extends NodeScala {
+    self =>
+    val listeners = mutable.Map[String, DummyListener]()
+
+    def createListener(relativePath: String) = {
+      val l = new DummyListener(port, relativePath)
+      listeners(relativePath) = l
+      l
+    }
+
+    def emit(relativePath: String, req: Request) = this.synchronized {
+      val l = listeners(relativePath)
+      l.emit(req)
+    }
+  }
+
+  test("Listener should serve the next request as a future") {
+    val dummy = new DummyListener(8191, "/test")
+    val subscription = dummy.start()
+
+    def test(req: Request) {
+      val f = dummy.nextRequest()
+      dummy.emit(req)
+      val (reqReturned, xchg) = Await.result(f, 1 second)
+
+      assert(reqReturned == req)
+    }
+
+    test(immutable.Map("StrangeHeader" -> List("StrangeValue1")))
+    test(immutable.Map("StrangeHeader" -> List("StrangeValue2")))
+
+    subscription.unsubscribe()
+  }
+
+  test("Server should serve requests") {
+    val dummy = new DummyServer(8191)
+    val dummySubscription = dummy.start("/testDir") {
+      request => for (kv <- request.iterator) yield (kv + "\n").toString
+    }
+
+    // wait until server is really installed
+    Thread.sleep(500)
+
+    def test(req: Request) {
+      val webpage = dummy.emit("/testDir", req)
+      val content = Await.result(webpage.loaded.future, 1 second)
+      val expected = (for (kv <- req.iterator) yield (kv + "\n").toString).mkString
+      assert(content == expected, s"'$content' vs. '$expected'")
+    }
+
+    test(immutable.Map("StrangeRequest" -> List("Does it work?")))
+    test(immutable.Map("StrangeRequest" -> List("It works!")))
+    test(immutable.Map("WorksForThree" -> List("Always works. Trust me.")))
+
+    dummySubscription.unsubscribe()
+  }
+
+}
+
+
+
+
